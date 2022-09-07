5ba8ca3d81c8f79d1055242d4e040cd68acdb28a
Edward Ramirez <Edward.Ramirez@coxautoinc.com>
2022-09-06 22:32:43 -0500

    feat: handle semantic release and npm upload setup at project bootstrap script

--- setup.sh
+++ setup.sh
@@ -16,8 +16,81 @@ done
 
 repository=""
 currentRepo="https://github.com/edwardramirez31/micro-frontend-utility-module"
-read -p "🔷 Enter your GitHub repository URL name to add semantic release: " repository
+read -p "🔷 Enter your GitHub repository URL: " repository
 sed -i "s,$currentRepo,$repository,g" .releaserc
+sed -i "s,$currentRepo,$repository,g" package.json
+
+semanticReleaseRemoved=false
+while true; do
+    read -p "🔷 Do you want to use Semantic Release? [y/N]: " yn
+    case $yn in
+        [Yy]* )
+          echo "⚠️  Don't forget setting ACTIONS_DEPLOY_ACCESS_TOKEN secret at your repository"
+          break
+        ;;
+        [Nn]* )
+          rm .releaserc
+          sed -i.bak -e '44,48d' .github/workflows/main.yml && rm .github/workflows/main.yml.bak
+          sed -i.bak -e '38,40d' package.json && rm package.json.bak
+          semanticReleaseRemoved=true
+          break
+        ;;
+        * ) echo "Please answer yes or no like: [y/N]";;
+    esac
+done
+
+npmRemoved=false
+while true; do
+    read -p "🔷 Do you want to release this utility module to NPM? [y/N]: " yn
+    case $yn in
+        [Yy]* )
+          echo "⚠️  Don't forget setting NPM_TOKEN secret at your repository with an NPM Automation Access Token so that your utility can be deployed to NPM"
+          echo "💡 Go ahead and change the project author, description, keywords and license at package.json according to your needs"
+          break
+        ;;
+        [Nn]* )
+          rm .npmignore
+          linesToRemove="49,56d"
+          if [[ "$semanticReleaseRemoved" == true ]]; then
+            linesToRemove="44,51d"
+          fi
+          sed -i.bak -e $linesToRemove .github/workflows/main.yml && rm .github/workflows/main.yml.bak
+          npmRemoved=true
+          break
+        ;;
+        * ) echo "Please answer yes or no like: [y/N]";;
+    esac
+done
+
+while true; do
+    read -p "🔷 Do you want to deploy this utility module to S3? [y/N]: " yn
+    case $yn in
+        [Yy]* )
+          bucketValidation=^[a-z0-9.-]+$
+          bucketName=""
+          while ! [[ "${bucketName?}" =~ ${bucketValidation} ]]
+          do
+            read -p "🔷 Enter your S3 Bucket Name: " bucketName
+          done
+          sed -i "s/mf-todo/$bucketName/g" .github/workflows/main.yml
+          echo "⚠️  Don't forget to setup bucket access and ACL so that the root module can get your build file"
+          break
+        ;;
+        [Nn]* )
+          linesToRemove="57,68d"
+          if [[ "$semanticReleaseRemoved" == true ]] && [[ "$npmRemoved" == true ]]; then
+            linesToRemove="44,55d"
+          elif [[ "$semanticReleaseRemoved" == true ]]; then
+            linesToRemove="52,63d"
+          elif [[ "$npmRemoved" == true ]]; then
+            linesToRemove="49,60d"
+          fi
+          sed -i.bak -e $linesToRemove .github/workflows/main.yml && rm .github/workflows/main.yml.bak
+          break
+        ;;
+        * ) echo "Please answer yes or no like: [y/N]";;
+    esac
+done
 
 sed -i "s/my-app/$project/g" package.json
 sed -i "s/utility/$service/g" package.json
